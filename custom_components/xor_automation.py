6707d8ff458cabbf531217b6d22291e739d058c9
Dan Dietz <danddietz@gmail.com>
2017-05-09 10:56:52 -0400

    added xor automation

---
+++ custom_components/xor_automation.py
@@ -0,0 +1,26 @@
+from homeassistant.helpers.event import track_state_change
+from homeassistant.components import is_on, toggle
+
+DOMAIN = 'xor_automation'
+
+
+def setup(hass, config):
+    entity_1 = config[DOMAIN]['entity_1']
+    entity_2 = config[DOMAIN]['entity_2']
+
+    def state_changed(entity_id, old_state, new_state):
+        if entity_id == entity_1:
+            other = entity_2
+        else:
+            other = entity_1
+
+        if is_on(hass, entity_id) == is_on(hass, other):
+            toggle(hass, other)
+
+    # Ensure current state is opposite
+    if is_on(hass, entity_1) == is_on(hass, entity_2):
+        toggle(hass, entity_2)
+
+    track_state_change(hass, [entity_1, entity_2], state_changed)
+
+    return True
